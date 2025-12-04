7c83ad1de29b0f00dab0d1d6746c898f1598f8a9
Anha Alishba Dahri <anhaalishbah@gmail.com>
2025-12-04 13:05:43 +0500

    Update app.py

--- iris_prediction_app/app_streamlit/app.py
+++ iris_prediction_app/app_streamlit/app.py
@@ -1,20 +1,19 @@
+import os
+from dotenv import load_dotenv
 import streamlit as st
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+load_dotenv()
+
+API_URL = os.getenv("API_URL") 
 
 st.set_page_config(page_title="Iris Flower Classifier 🌸")
 
 st.title("Iris Flower Classification 🌸")
 st.write("Enter the flower measurements below and see the predicted species along with charts.")
 
-# -------------------------
-# API URL Input
-# -------------------------
-API_URL = "http://127.0.0.1:8000/predict"
-# -------------------------
 # User Inputs
-# -------------------------
 with st.form("iris_form"):
     sepal_length = st.number_input("Sepal Length (cm)", min_value=0.0, max_value=10.0, value=5.1)
     sepal_width = st.number_input("Sepal Width (cm)", min_value=0.0, max_value=10.0, value=3.5)
@@ -38,9 +37,8 @@ if submitted:
             prediction = data['prediction']
             st.success(f"Predicted Species: **{prediction}**")
 
-            # -------------------------
-            # Probability Charts
-            # -------------------------
+           
+            #  Chart
             if 'probabilities' in data and data['probabilities']:
                 st.subheader("Prediction Probabilities 📊")
                 prob_df = pd.DataFrame.from_dict(data['probabilities'], orient='index', columns=['Probability'])
@@ -52,3 +50,4 @@ if submitted:
 
     except Exception as e:
         st.error(f"Error connecting to API: {e}")
+
