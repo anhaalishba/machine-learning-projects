a0aabbcc3b97d63691ce722197b9f7279129eb8c
Anha Alishba Dahri <anhaalishbah@gmail.com>
2025-12-04 13:03:33 +0500

    Update main.py

--- iris_prediction_app/app_fastapi/main.py
+++ iris_prediction_app/app_fastapi/main.py
@@ -5,15 +5,12 @@ import pandas as pd
 
 app = FastAPI(title="Iris Classification API")
 
-# -------------------------------
 # Load trained model
-# -------------------------------
 MODEL_PATH = "best_pipeline.pkl"
 model = joblib.load(MODEL_PATH)
 
-# -------------------------------
-# Pydantic schema for input validation
-# -------------------------------
+# Pydantic  for input validation
+
 class IrisInput(BaseModel):
     SepalLengthCm: float
     SepalWidthCm: float
@@ -26,17 +23,15 @@ class IrisInput(BaseModel):
             raise ValueError('Value must be between 0 and 10')
         return v
 # Predict endpoint
-# -------------------------------
+
 @app.post("/predict")
 def predict(payload: IrisInput):
     try:
-        # Convert input to DataFrame
+       
         X = pd.DataFrame([payload.dict()])
 
-        # Predict class
         pred = model.predict(X)[0]
 
-        # Predict probabilities
         proba = model.predict_proba(X)[0].tolist() if hasattr(model, "predict_proba") else None
         classes = model.classes_.tolist() if hasattr(model, "classes_") else None
 
@@ -48,9 +43,7 @@ def predict(payload: IrisInput):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# -------------------------------
-# Health check
-# -------------------------------
 @app.get("/health")
 def health():
     return {"status": "ok", "model_loaded": model is not None}
+
